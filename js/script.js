dd6718740a90488954553eca519f2a87f7cb35dd
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-09-15 14:40:42 +0200

    JS - DOM.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-
-const color = 'niebieski' 
-//zmienna stała, której nie mozemy przypisać innej wartości.
-
-let age = 26
-// zmienna, której mozemy przypisywać inną wartość. 
-
-console.log(age);
-
-const name = "Justyna"
-
-console.log(`Czesc mam na imię ${name} i mam ${age} lat.`);
+const p = document.createElement('p')
+p.textContent = 'I am a dynamically added element'
+document.body.append(p)
