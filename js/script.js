d4551cfc7f5beac82bfee22b3b103cb16df68fa8
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-09-15 13:51:06 +0200

    JS - variables.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("Hello!");
+
+const color = 'niebieski' 
+//zmienna stała, której nie mozemy przypisać innej wartości.
+
+let age = 26
+// zmienna, której mozemy przypisywać inną wartość. 
+
+console.log(age);
+
+const name = "Justyna"
+
+console.log(`Czesc mam na imię ${name} i mam ${age} lat.`);
