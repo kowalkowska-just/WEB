ed9fc6ec0392fc56c4c7e78267701c92adeec700
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-09-15 15:59:20 +0200

    JS - Task solution.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
- //Logical operators
+// TASK Java Script
 
-/*
+let score 
 
-&& -> and 
-|| -> or 
-! -> negation
-
- */
-let x = true
-let y = false
-
-if (x != y ) {
-    console.log(`🟢`)
+if ( 15 % 2 == 1) {
+    console.log(`SCORE is not even`)
 } else {
-    console.log(`🔴`)
+    console.log(`SCORE is even.`);
 }
+
+// ---------------------------------
+
+let num1 = 8 
+num1++ 
+num1++ 
+
+console.log(num1);
+
+// --------------------------------
+
+let x = 5 
+let y = 10
+
+console.log(x*=y)
+console.log(x/=y)
