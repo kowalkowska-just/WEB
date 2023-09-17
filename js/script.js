8cf646e32be0c9277aae45d4a22f6265f5932c6e
Justyna Kowalkowska <67439169+kowalkowska-just@users.noreply.github.com>
2023-09-17 10:22:05 +0200

    JS - Array.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-// IF 
-const passLength = 6
+// Arrays
 
-if (passLength >= 10) {
-    console.log("You have a very good password! :)");
-} else if (passLength > 5 && passLength < 10 ){
-    console.log("You have a good password.");
-} else {
-    console.log("You have a weak password. ;/");
-}
+const colors = ['red', 'green', 'blue']
+
+console.log(colors.length);
+
+colors.push('gold')
+
+console.log(colors.length);
